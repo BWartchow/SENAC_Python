6ef36bba10ee93559695239d3e7e71f2a6093379
Bê Wartchow <89083363+BWartchow@users.noreply.github.com>
2022-11-21 21:18:22 -0300

    Add files via upload

---
+++ Exercicios_Aula_03/Aula03_03.py
@@ -0,0 +1,9 @@
+# Escreva um programa que leia o nome de uma cidade e informe se ela começa com a palavra “Santo”.
+# Escreva um programa que leia o nome completo de uma pessoa e diga se ela tem “Silva” no nome.
+
+print("Cidade com Santo???")
+cidade = input("Digite o nome da cidade: ")
+print("Santo" in cidade)
+print("Nome com Silva?????")
+nome = input("Nome completo: ")
+print("Silva" in nome)
