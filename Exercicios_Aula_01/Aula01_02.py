25e619265b130e19133fb585be2bd5a2ea124b63
Bê Wartchow <89083363+BWartchow@users.noreply.github.com>
2022-11-21 21:06:27 -0300

    Add files via upload

---
+++ Exercicios_Aula_01/Aula01_02.py
@@ -0,0 +1,6 @@
+# Escreva um programa que leia o dia, o mês e o ano de nascimento do usuário e mostre uma mensagem formatada.
+
+dia = input("Em que dia você nasceu? ")
+mes = input("E em que mês? ")
+ano = input("Em qual ano? ")
+print("Você nasceu em:",dia,"/",mes,"/",ano)
