363b030c3ff4117fc54da2c58489bbee70c1d7d4
Bê Wartchow <89083363+BWartchow@users.noreply.github.com>
2022-11-21 21:05:15 -0300

    Add files via upload

---
+++ Exercicios_Aula_01/Aula01_01.py
@@ -0,0 +1,7 @@
+# Crie as varíaveis nome e idade, pergunte e mostre para o usuário.
+# Escreva um programa que leia o nome do usuário e mostre uma mensagem de boas-vindas para ele.
+
+nome = input('Qual o seu nome? ')
+idade = input('Qual sua idade? ')
+print('O',nome,'tem', idade, 'anos. \nOlá,',nome,'!')
+
