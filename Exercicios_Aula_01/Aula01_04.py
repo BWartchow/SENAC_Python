0696a5b9c620dfb6c7f0e64db331efa07e097fc9
Bê Wartchow <89083363+BWartchow@users.noreply.github.com>
2022-11-21 21:07:16 -0300

    Add files via upload

---
+++ Exercicios_Aula_01/Aula01_04.py
@@ -0,0 +1,6 @@
+# Escreva um programa que leia um número e mostre seu antecessor e seu posterior.
+
+n = int(input("Digite um número inteiro: "))
+antecessor = n-1
+sucessor = n+1
+print("O antecessor de {} é {} e o sucessor é {}".format(n,antecessor,sucessor))
