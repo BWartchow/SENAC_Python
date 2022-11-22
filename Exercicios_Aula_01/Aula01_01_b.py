69d664fafdbe9912aba082c31c1c469cb23e2ed0
Bê Wartchow <89083363+BWartchow@users.noreply.github.com>
2022-11-21 21:06:05 -0300

    Add files via upload

---
+++ Exercicios_Aula_01/Aula01_01_b.py
@@ -0,0 +1,13 @@
+nome = "Fulano"
+print(f"Prazer, {nome:20}")
+print(f"Prazer, {nome:>20}")
+print(f"Prazer, {nome:<20}")
+print(f"Prazer, {nome:^20}")
+print(f"Prazer, {nome:=^20}")
+
+print("texto ", end=' ')
+print(" mais texto")
+
+import math
+
+print(f"Exibir potencia: {math.pow(2,2)}")
