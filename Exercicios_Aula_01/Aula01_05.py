a51a992d78d4112ab8329a018d8f78dab5e90831
Bê Wartchow <89083363+BWartchow@users.noreply.github.com>
2022-11-21 21:07:48 -0300

    Add files via upload

---
+++ Exercicios_Aula_01/Aula01_05.py
@@ -0,0 +1,4 @@
+# Escreva um programa que leia um número e mostre o dobro, o triplo e raíz quadrada.
+
+N = int(input("Digite um valor = "))
+print("Dobro = {}\nTriplo = {}\nRaiz quadrada = {}".format(2*N,3*N,(N**(1/2))))
