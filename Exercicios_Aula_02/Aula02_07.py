c416ea1599960cddd747f36526dae7f72e501ce3
Bê Wartchow <89083363+BWartchow@users.noreply.github.com>
2022-11-22 00:30:32 -0300

    Add files via upload

---
+++ Exercicios_Aula_02/Aula02_07.py
@@ -0,0 +1,7 @@
+# Módulo de datas:
+
+import datetime
+print(f"A data de hoje é {datetime.date.today()}")
+print(f"Dia: {datetime.date.today().day}")
+print(f"Mês: {datetime.date.today().month}")
+print(f"Ano: {datetime.date.today().year}")
